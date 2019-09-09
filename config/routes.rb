c0d9efc92b8e63949c86380ee5842768f49b5cb7
MichelleNguy <plasticwatering@gmail.com>
2019-09-09 16:57:48 -0400

    Added User show page, route for portfolio page, and began portfolio view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :cryptos
   resources :users
   resources :exchanges
+  get "/users/:id/portfolio", to:"users#portfolio"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

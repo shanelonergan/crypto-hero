107b351826cc075f997d95d0ee7f3f0fafc60651
MichelleNguy <plasticwatering@gmail.com>
2019-09-09 16:28:19 -0400

    Removed investment_total from Users table. Added set_user and user_params method to User controller. Added before_action macros.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:delete, :update, :edit, :show]
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :email,
+            :password,
+            :bio, 
+            :photo_url,
+            :balance
+        )
+    end
+
 end

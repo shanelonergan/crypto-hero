67e704969237cb03165183b26feacdb4bb12ef69
MichelleNguy <plasticwatering@gmail.com>
2019-09-10 13:19:29 -0400

    Changed name coulmn to username and password column to password_digest in Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_132817) do
+ActiveRecord::Schema.define(version: 2019_09_10_171812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 2019_09_10_132817) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "bio"
     t.string "photo_url"
     t.float "balance"
